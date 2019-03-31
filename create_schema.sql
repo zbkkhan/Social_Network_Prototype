fda4adc6b3fa368ad95ba6e590d563c10fbd1c33
Zain Khan <zkworld1234@gmail.com>
2019-03-31 16:49:05 -0400

    SQL Files

---
+++ create_schema.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE social_network;
+use social_network;
+
+CREATE TABLE IF NOT EXISTS Person(
+	userID INT AUTO_INCREMENT,
+	firstName VARCHAR(30) NOT NULL,
+	lastName VARCHAR(30) NOT NULL,
+	birthDate DATE,
+	lastReadFeedAt TIMESTAMP NOT NULL DEFAULT '2000-03-31 00:00:00',
+	PRIMARY KEY (userId)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Friend_Group(
+	groupID INT AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL,
+	PRIMARY KEY (groupID)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Group_Member(
+	groupID INT NOT NULL,
+	memberID INT NOT NULL,
+	FOREIGN KEY fk_group(groupID) REFERENCES Friend_Group(groupID),
+	FOREIGN KEY fk_member(memberID) REFERENCES Person(userID)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Nested_Topic(
+	topicID INT AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL,
+	lft INT NOT NULL,
+	rgt INT NOT NULL,
+	PRIMARY KEY (topicID)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Following(
+	userID INT NOT NULL,
+	topicID INT NOT NULL,
+	FOREIGN KEY fk_following_user(userID) REFERENCES Person(userID),
+	FOREIGN KEY fk_following_topic(topicID) REFERENCES Nested_Topic(topicID)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Post(
+	postID INT AUTO_INCREMENT,
+	userID INT NOT NULL,
+	topicID INT NOT NULL,
+	createdAt TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	postText VARCHAR(255) NOT NULL,
+	PRIMARY KEY (postID),
+	FOREIGN KEY fk_post_user(userID) REFERENCES Person(userID),
+	FOREIGN KEY fk_post_topic(topicID) REFERENCES Nested_Topic(topicID)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Response(
+	responseID INT NOT NULL,
+	parentPostID INT NOT NULL,
+	FOREIGN KEY fk_response_post_res(responseID) REFERENCES Post(postID),
+	FOREIGN KEY fk_response_post_parent(parentPostID) REFERENCES Post(postID)
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS Engagement(
+	userID INT NOT NULL,
+	postID INT NOT NULL,
+	isThumbsUp BOOL NOT NULL,
+	FOREIGN KEY fk_like_user(userID) REFERENCES Person(userID),
+	FOREIGN KEY fk_like_post(postID) REFERENCES Post(postID)
+) ENGINE=INNODB;
---
+++ populate_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Nested_Topic VALUES(1, 'HEALTH', 1, 6), (2, 'KETO',  2, 3), (3, 'PALEO', 4, 5), (4, 'PROGRAMMING', 7, 12), (5, 'C++', 8, 9), (6, 'JAVA', 10, 11);
+
+INSERT INTO Friend_Group VALUES(1, 'HEALTH_GROUP'),(2, 'TECH_GROUP');
